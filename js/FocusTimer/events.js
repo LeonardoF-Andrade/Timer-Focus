d94c9634a52b2a78ea1a28a200a975e7987ef799
Leonardo-FAnddrade <leonardo.andrade99@hotmail.com>
2024-06-19 02:01:40 -0300

    Final commit

--- js/FocusTimer/events.js
+++ js/FocusTimer/events.js
@@ -1,33 +1,32 @@
 import { controls } from "./elements.js";
 import * as actions from "./actions.js";
 import * as el from "./elements.js";
-import state from './state.js';
-import { updateDisplay } from './timer.js';
-
+import state from "./state.js";
+import { updateDisplay } from "./timer.js";
 
 export function registerControls() {
   controls.addEventListener("click", (event) => {
     const action = event.target.dataset.action;
-    if (typeof(actions[action]) != 'function') return;
+    if (typeof actions[action] != "function") return;
     actions[action]();
   });
 }
 
-export function setMinutes(){
-  el.minutes.addEventListener('focus', () => {
-    el.minutes.textContent = '';
-  })
+export function setMinutes() {
+  el.minutes.addEventListener("focus", () => {
+    el.minutes.textContent = "";
+  });
 
-  el.minutes.onkeypress = (event) => /\d/.test(event.key)// '\d' para verificar se é numero, se for ele retorna true
+  el.minutes.onkeypress = (event) => /\d/.test(event.key); // '\d' para verificar se é numero, se for ele retorna true
 
-  el.minutes.addEventListener('blur', () => {
+  el.minutes.addEventListener("blur", () => {
     let time = event.currentTarget.textContent;
     time = time > 60 ? 60 : time;
     state.minutes = time;
     state.seconds = 0;
-    updateDisplay()
-    el.minutes.removeAttribute('contenteditable')
-  })
+    updateDisplay();
+    el.minutes.removeAttribute("contenteditable");
+  });
 }
 
 // export function setSeconds(){
